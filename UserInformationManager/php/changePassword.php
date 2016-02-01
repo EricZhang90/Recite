ccf5763cb0b7e5cfed8bbf28c0dfdbbe8b6d6d26
Eric <lzhang216@myseneca.ca>
2016-01-31 21:49:27 -0500

    Create changePassword.php

---
+++ UserInformationManager/php/changePassword.php
@@ -0,0 +1,34 @@
+<?php
+    if($_GET['password']){
+        session_start();
+        $_SESSION['oldPassword'] = $_GET['password'];
+    }
+
+    if(isset($_POST['newPassword'])){
+        include_once("MyDB.php");
+
+        $myDB = new MyDB();
+        $myDB->modifyDB("update userProfile set password ='" . md5($_POST['newPassword']) . "' where password ='" . $_SESSION['oldPassword'] . "'");
+        session_destroy();
+        header("Location: login.php");
+    }
+?>
+
+
+<html>
+    <body>
+        <form method="post">
+            <table>
+                <tr>
+                    <td>set a new password: </td>
+                    <td><input type="password" name="newPassword"></td>
+                </tr>
+                <tr>
+                    <td><button type="submit">change password</button></td>
+                </tr>
+            </table>
+            <br/>
+        </form>
+    </body>
+</html>
+
